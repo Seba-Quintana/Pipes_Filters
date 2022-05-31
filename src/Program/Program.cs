a92e269ca14892d4c053e4d0d47f36c987a518eb
Seba-Quintana <SEBASTIAN.QUINTANA@correo.ucu.edu.uy>
2022-05-31 20:39:33 -0300

    net 6.0 y ejercicio 1

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,6 +8,18 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture(@"luke.jpg");
+            IFilter filterNegative = new FilterNegative();
+            IPipe pipeNull = new PipeNull();
+            IPipe pipe2 = new PipeSerial(filterNegative, pipeNull);
+            IPicture image2 = pipe2.Send(picture);
+            provider.SavePicture(image2, @"luke.jpg");
+            IFilter filterGreyscale = new FilterGreyscale();
+            IPipe pipe = new PipeSerial(filterGreyscale, pipe2);
+            IPicture image = pipe.Send(picture);
+            provider.SavePicture(image, @"luke.jpg");
         }
     }
 }
